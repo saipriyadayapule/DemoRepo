ff47bae2adb42a9c51ec775adf2596112a66b4c9
Shinu Suresh <mailtoshinu@gmail.com>
2018-07-24 22:40:59 +0100

    added search page feature code

--- src/test/java/com/qa/testpage/LoginTest.java
+++ src/test/java/com/qa/testpage/LoginTest.java
@@ -1,9 +1,16 @@
 package com.qa.testpage;
 
+import org.testng.annotations.Test;
+
 public class LoginTest {
-	
+	@Test
 	public void test1() {
 		System.out.println("test1");
 	}
 
+	
+	public void page() {
+		System.out.println("log it");
+	}
+	
 }
